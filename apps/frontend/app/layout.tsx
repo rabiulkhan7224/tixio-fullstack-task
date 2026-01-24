367236fd92a41876dc00025552f2dbaac095b454
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-01-24 10:17:54 +0600

    Frontend: usercrete hook added and setup tq

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ReactQueryProvider>
+          
          <Header />
         {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
