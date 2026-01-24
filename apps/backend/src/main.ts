7d23eac4eaef6858253ee6f1b0c05f31137f16d8
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-01-24 14:37:20 +0600

    backend: fixed build error and cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,6 +7,13 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+
+ app.enableCors({
+    origin: ['http://localhost:3000', 'https://frontend-production-97bc.up.railway.app'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   // Global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
